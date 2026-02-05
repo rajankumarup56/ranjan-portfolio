01dc7839a78ea283948718da8e24b61d6174ae0f
rajankumarup56 <rajankumarup56@zohomail.in>
2026-02-05 23:56:36 +0530

    Fresh portfolio upload

--- js/toggle.js
+++ js/toggle.js
@@ -1,20 +1,27 @@
 const devopsBtn = document.getElementById("devopsBtn");
 const backendBtn = document.getElementById("backendBtn");
+
 const devopsTrack = document.getElementById("devops-track");
 const backendTrack = document.getElementById("backend-track");
 
 devopsBtn.addEventListener("click", () => {
-  devopsBtn.classList.add("active");
-  backendBtn.classList.remove("active");
   devopsTrack.classList.remove("hidden");
   devopsTrack.classList.add("visible");
+
+  backendTrack.classList.remove("visible");
   backendTrack.classList.add("hidden");
+
+  devopsBtn.classList.add("active");
+  backendBtn.classList.remove("active");
 });
 
 backendBtn.addEventListener("click", () => {
-  backendBtn.classList.add("active");
-  devopsBtn.classList.remove("active");
   backendTrack.classList.remove("hidden");
   backendTrack.classList.add("visible");
+
+  devopsTrack.classList.remove("visible");
   devopsTrack.classList.add("hidden");
+
+  backendBtn.classList.add("active");
+  devopsBtn.classList.remove("active");
 });
