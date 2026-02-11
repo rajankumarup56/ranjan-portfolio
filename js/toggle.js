b164f17b41fe93f1561ec29ffedd8be4e8367850
Ranjan Kumar Upadhyay <rajankumarup56@zohomail.in>
2026-02-11 23:52:45 +0530

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -1,26 +1,22 @@
-<!-- JS -->
-  <script>
-    const devBtn = document.getElementById("devopsBtn");
-    const backBtn = document.getElementById("backendBtn");
-    const devTrack = document.getElementById("devops-track");
-    const backTrack = document.getElementById("backend-track");
+const devBtn = document.getElementById("devopsBtn");
+const backBtn = document.getElementById("backendBtn");
+const devTrack = document.getElementById("devops-track");
+const backTrack = document.getElementById("backend-track");
 
-    devBtn.onclick = () => {
-      devBtn.classList.add("active");
-      backBtn.classList.remove("active");
-      devTrack.classList.add("visible");
-      devTrack.classList.remove("hidden");
-      backTrack.classList.add("hidden");
-    };
+devBtn.onclick = () => {
+  devBtn.classList.add("active");
+  backBtn.classList.remove("active");
+  devTrack.classList.add("visible");
+  devTrack.classList.remove("hidden");
+  backTrack.classList.add("hidden");
+  backTrack.classList.remove("visible");
+};
 
-    backBtn.onclick = () => {
-      backBtn.classList.add("active");
-      devBtn.classList.remove("active");
-      backTrack.classList.add("visible");
-      backTrack.classList.remove("hidden");
-      devTrack.classList.add("hidden");
-    };
-  </script>
-
-</body>
-</html>
+backBtn.onclick = () => {
+  backBtn.classList.add("active");
+  devBtn.classList.remove("active");
+  backTrack.classList.add("visible");
+  backTrack.classList.remove("hidden");
+  devTrack.classList.add("hidden");
+  devTrack.classList.remove("visible");
+};
