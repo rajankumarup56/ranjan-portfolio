c6f1465d37384d1775cb9be9b45f4eced827de12
Ranjan Kumar Upadhyay <rajankumarup56@zohomail.in>
2026-03-19 09:03:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const elements = document.querySelectorAll('.fade-in');
-
-window.addEventListener('scroll', () => {
-elements.forEach(el => {
-const position = el.getBoundingClientRect().top;
-if (position < window.innerHeight - 100) {
-el.classList.add('show');
-}
-});
-});
+console.log("Portfolio Loaded 🚀");
