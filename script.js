907c59bb9fba955ad46624f859509e61524f90df
Ranjan Kumar Upadhyay <rajankumarup56@zohomail.in>
2026-03-19 20:36:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Portfolio Loaded 🚀");
+
+// Scroll animation
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    const position = section.getBoundingClientRect().top;
+
+    if (position < window.innerHeight - 100) {
+      section.classList.add("show");
+    }
+  });
+});
